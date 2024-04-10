a0f76d630fa463177d661c19cc5d7fe889e13bbe
manuelgaticapaz <manuel.gatica.paz.94@gmail.com>
2024-04-09 22:01:07 -0600

    Más menus

--- ProyectoFinal/Form1.Designer.cs
+++ ProyectoFinal/Form1.Designer.cs
@@ -32,6 +32,29 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.reservacionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.habitacionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevaReservaciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editarReservaciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.checkInToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.serviciosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listaDeReservacionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.huespedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listadoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevaHabitaciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editarHabitaciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.tiposDeHabitaciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.editarToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.verTodoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.disponibilidadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.habilitarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deshabilitarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -39,7 +62,8 @@
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.reservacionesToolStripMenuItem,
-            this.habitacionesToolStripMenuItem});
+            this.habitacionesToolStripMenuItem,
+            this.serviciosToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(800, 24);
@@ -48,15 +72,176 @@
             // 
             // reservacionesToolStripMenuItem
             // 
+            this.reservacionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevaReservaciónToolStripMenuItem,
+            this.editarReservaciónToolStripMenuItem,
+            this.listaDeReservacionesToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.checkInToolStripMenuItem,
+            this.checkOutToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.huespedToolStripMenuItem});
             this.reservacionesToolStripMenuItem.Name = "reservacionesToolStripMenuItem";
             this.reservacionesToolStripMenuItem.Size = new System.Drawing.Size(93, 20);
             this.reservacionesToolStripMenuItem.Text = "Reservaciones";
             // 
             // habitacionesToolStripMenuItem
             // 
+            this.habitacionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevaHabitaciónToolStripMenuItem,
+            this.editarHabitaciónToolStripMenuItem,
+            this.toolStripSeparator4,
+            this.disponibilidadToolStripMenuItem,
+            this.toolStripSeparator3,
+            this.tiposDeHabitaciónToolStripMenuItem});
             this.habitacionesToolStripMenuItem.Name = "habitacionesToolStripMenuItem";
             this.habitacionesToolStripMenuItem.Size = new System.Drawing.Size(88, 20);
             this.habitacionesToolStripMenuItem.Text = "Habitaciones";
+            // 
+            // nuevaReservaciónToolStripMenuItem
+            // 
+            this.nuevaReservaciónToolStripMenuItem.Name = "nuevaReservaciónToolStripMenuItem";
+            this.nuevaReservaciónToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.nuevaReservaciónToolStripMenuItem.Text = "Nueva Reservación";
+            // 
+            // editarReservaciónToolStripMenuItem
+            // 
+            this.editarReservaciónToolStripMenuItem.Name = "editarReservaciónToolStripMenuItem";
+            this.editarReservaciónToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.editarReservaciónToolStripMenuItem.Text = "Editar Reservación";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(188, 6);
+            // 
+            // checkInToolStripMenuItem
+            // 
+            this.checkInToolStripMenuItem.Name = "checkInToolStripMenuItem";
+            this.checkInToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.checkInToolStripMenuItem.Text = "Check-In";
+            // 
+            // checkOutToolStripMenuItem
+            // 
+            this.checkOutToolStripMenuItem.Name = "checkOutToolStripMenuItem";
+            this.checkOutToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.checkOutToolStripMenuItem.Text = "Check-Out";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(188, 6);
+            // 
+            // serviciosToolStripMenuItem
+            // 
+            this.serviciosToolStripMenuItem.Name = "serviciosToolStripMenuItem";
+            this.serviciosToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.serviciosToolStripMenuItem.Text = "Servicios";
+            // 
+            // listaDeReservacionesToolStripMenuItem
+            // 
+            this.listaDeReservacionesToolStripMenuItem.Name = "listaDeReservacionesToolStripMenuItem";
+            this.listaDeReservacionesToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.listaDeReservacionesToolStripMenuItem.Text = "Lista de Reservaciones";
+            // 
+            // huespedToolStripMenuItem
+            // 
+            this.huespedToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoToolStripMenuItem,
+            this.editarToolStripMenuItem,
+            this.listadoToolStripMenuItem});
+            this.huespedToolStripMenuItem.Name = "huespedToolStripMenuItem";
+            this.huespedToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.huespedToolStripMenuItem.Text = "Huesped";
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nuevoToolStripMenuItem.Text = "Nuevo";
+            // 
+            // editarToolStripMenuItem
+            // 
+            this.editarToolStripMenuItem.Name = "editarToolStripMenuItem";
+            this.editarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.editarToolStripMenuItem.Text = "Editar";
+            // 
+            // listadoToolStripMenuItem
+            // 
+            this.listadoToolStripMenuItem.Name = "listadoToolStripMenuItem";
+            this.listadoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.listadoToolStripMenuItem.Text = "Listado";
+            // 
+            // nuevaHabitaciónToolStripMenuItem
+            // 
+            this.nuevaHabitaciónToolStripMenuItem.Name = "nuevaHabitaciónToolStripMenuItem";
+            this.nuevaHabitaciónToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nuevaHabitaciónToolStripMenuItem.Text = "Nueva Habitación";
+            // 
+            // editarHabitaciónToolStripMenuItem
+            // 
+            this.editarHabitaciónToolStripMenuItem.Name = "editarHabitaciónToolStripMenuItem";
+            this.editarHabitaciónToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.editarHabitaciónToolStripMenuItem.Text = "Editar Habitación";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(177, 6);
+            // 
+            // tiposDeHabitaciónToolStripMenuItem
+            // 
+            this.tiposDeHabitaciónToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoToolStripMenuItem1,
+            this.editarToolStripMenuItem1,
+            this.verTodoToolStripMenuItem});
+            this.tiposDeHabitaciónToolStripMenuItem.Name = "tiposDeHabitaciónToolStripMenuItem";
+            this.tiposDeHabitaciónToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.tiposDeHabitaciónToolStripMenuItem.Text = "Tipos de Habitación";
+            // 
+            // nuevoToolStripMenuItem1
+            // 
+            this.nuevoToolStripMenuItem1.Name = "nuevoToolStripMenuItem1";
+            this.nuevoToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.nuevoToolStripMenuItem1.Text = "Nuevo";
+            // 
+            // editarToolStripMenuItem1
+            // 
+            this.editarToolStripMenuItem1.Name = "editarToolStripMenuItem1";
+            this.editarToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.editarToolStripMenuItem1.Text = "Editar";
+            // 
+            // verTodoToolStripMenuItem
+            // 
+            this.verTodoToolStripMenuItem.Name = "verTodoToolStripMenuItem";
+            this.verTodoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.verTodoToolStripMenuItem.Text = "Ver Todo";
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(177, 6);
+            // 
+            // disponibilidadToolStripMenuItem
+            // 
+            this.disponibilidadToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.habilitarToolStripMenuItem,
+            this.deshabilitarToolStripMenuItem});
+            this.disponibilidadToolStripMenuItem.Name = "disponibilidadToolStripMenuItem";
+            this.disponibilidadToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.disponibilidadToolStripMenuItem.Text = "Disponibilidad";
+            // 
+            // habilitarToolStripMenuItem
+            // 
+            this.habilitarToolStripMenuItem.Name = "habilitarToolStripMenuItem";
+            this.habilitarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.habilitarToolStripMenuItem.Text = "Habilitar ";
+            // 
+            // deshabilitarToolStripMenuItem
+            // 
+            this.deshabilitarToolStripMenuItem.Name = "deshabilitarToolStripMenuItem";
+            this.deshabilitarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.deshabilitarToolStripMenuItem.Text = "Deshabilitar";
             // 
             // Form1
             // 
@@ -82,6 +267,29 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem reservacionesToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem habitacionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevaReservaciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editarReservaciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem checkInToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkOutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem serviciosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listaDeReservacionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem huespedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listadoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevaHabitaciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editarHabitaciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripMenuItem disponibilidadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem tiposDeHabitaciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevoToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem editarToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem verTodoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem habilitarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deshabilitarToolStripMenuItem;
     }
 }
 
